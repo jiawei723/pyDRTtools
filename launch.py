3e7f1ea558aaf6de56b822ebb08e258a72320fc4
miaojw <miaojiawei@dp.tech>
2024-05-17 16:25:28 +0800

    0517

--- launch.py
+++ launch.py
@@ -23,14 +23,37 @@ def launch_gui():
 
     # Sidebar widgets
     uploaded_file = st.sidebar.file_uploader("Choose a CSV or TXT file", type=["csv", "txt"])
-    induct_choice = st.sidebar.selectbox("Inductance Choice", ["", "Option 1", "Option 2"])
+    induct_options = ["Fitting w/o Inductance", "Fitting with Inductance", "Discard Inductive Data"]
+    induct_choice = st.sidebar.selectbox("Inductance Included", induct_options)
+
+    if induct_choice == "Fitting w/o Inductance" :
+        induct_used = 1
+    elif induct_choice == "Fitting with Inductance" :
+        induct_used = 2
+    else:
+        induct_used = 0
+
+    # Processing options
+    process_options = ["Simple Run", "Bayesian Run", "BHT Run", "Peak Analysis Run"]
+    selected_process = st.selectbox("Select Processing", process_options)
+
+    # Processing parameters
+    rbf_type = st.selectbox("RBF Type", ["Gaussian", "C0 Matern", "C2 Matern", "C4 Matern", "C6 Matern", "Inverse Quadratic", "Inverse Quadric", "Cauchy"])
+    data_used = st.selectbox("Data Used", ["Combined Re-Im Data", "Re Data", "Im Data"])
+    der_used = st.selectbox("Regularization Derivative", ["1st order", "2nd order"])
+    cv_type = st.selectbox("Regularization Method", ["custom", "GCV", "mGCV", "rGCV", "LC", "re-im", "kf"])
+    reg_param = st.number_input("Regularization Parameter", value=1e-3)
+    shape_control = st.selectbox("RBF Shape Control", ["FWHM Coefficient", "Shape Factor"])
+    coeff = st.number_input("FWHM Control", value=0.0)
+
+
     
     if uploaded_file is not None:
         # Read data
         data = EIS_object.from_file(uploaded_file)
 
         # Discard inductance data if necessary
-        if induct_choice == "Option 2":
+        if induct_choice == "Discard Inductive Data":
             data.freq = data.freq[-data.Z_double_prime > 0]
             data.Z_prime = data.Z_prime[-data.Z_double_prime > 0]
             data.Z_double_prime = data.Z_double_prime[-data.Z_double_prime > 0]
@@ -48,7 +71,7 @@ def launch_gui():
     # Main content
     if data is not None:
         # Display data
-        st.write(data)
+        # st.write(data)
 
         # Plot options
         plot_options = ["EIS_data", "Magnitude", "Phase", "Re_data", "Im_data", "Re_residual", "Im_residual", "DRT_data", "Score"]
@@ -66,39 +89,56 @@ def launch_gui():
             ax.set_xlabel('$f/Hz$')
             ax.set_ylabel('$|Z|/\Omega$')
         # Add more plotting options based on the original code
+        elif selected_plot == "Phase":
+            ax.semilogx(data.freq, angle(data.Z_exp), 'or')
+            ax.set_xlabel('$f/Hz$')
+            ax.set_ylabel('$\phi/deg$')
+        elif selected_plot == "Re_data":
+            ax.semilogx(data.freq, data.Z_prime, 'or')
+            ax.set_xlabel('$f/Hz$')
+            ax.set_ylabel('$Z^{\prime}/\Omega$')
+        elif selected_plot == "Im_data":
+            ax.semilogx(data.freq, data.Z_double_prime, 'or')
+            ax.set_xlabel('$f/Hz$')
+            ax.set_ylabel('$Z^{\prime \prime}/\Omega$')
+        elif selected_plot == "Re_residual":
+            ax.semilogx(data.freq, data.Z_prime - data.Z_prime_fit, 'or')
+            ax.set_xlabel('$f/Hz$')
+            ax.set_ylabel('$Z^{\prime}_{residual}/\Omega$')
+        elif selected_plot == "Im_residual":
+            ax.semilogx(data.freq, data.Z_double_prime - data.Z_double_prime_fit, 'or')
+            ax.set_xlabel('$f/Hz$')
+            ax.set_ylabel('$Z^{\prime \prime}_{residual}/\Omega$')
+        # elif selected_plot == "DRT_data":
+        #     ax.semilogx(data.out_tau_vec, data.gamma, 'or')
+        #     ax.set_xlabel(r'$\tau/s$')
+        #     ax.set_ylabel(r'$\gamma(log \tau)/\Omega$')
+        #     ax.set_ylim([0, 1.1 * max(data.gamma)])
+        #     ax.set_xlim([min(data.out_tau_vec), max(data.out_tau_vec)])
+        elif selected_plot == "Score":
+            ax.plot(data.score, 'or')
+            ax.set_xlabel('Iteration')
+            ax.set_ylabel('Score')
 
         st.pyplot(fig)
-
-        # Processing options
-        process_options = ["Simple Run", "Bayesian Run", "BHT Run", "Peak Analysis Run"]
-        selected_process = st.selectbox("Select Processing", process_options)
-
-        # Processing parameters
-        rbf_type = st.selectbox("RBF Type", ["Option 1", "Option 2"])
-        data_used = st.selectbox("Data Used", ["Option 1", "Option 2"])
-        der_used = st.selectbox("Derivative Used", ["Option 1", "Option 2"])
-        cv_type = st.selectbox("CV Type", ["Option 1", "Option 2"])
-        reg_param = st.number_input("Regularization Parameter", value=0.0)
-        shape_control = st.selectbox("Shape Control", ["Option 1", "Option 2"])
-        coeff = st.number_input("Coefficient", value=0.0)
 
         # Run processing
         if st.button("Run Processing"):
             if selected_process == "Simple Run":
-                data = simple_run(data, rbf_type=rbf_type, data_used=data_used, induct_used=induct_choice,
+                data = simple_run(data, rbf_type=rbf_type, data_used=data_used, induct_used=induct_used,
                                   der_used=der_used, cv_type=cv_type, reg_param=reg_param,
                                   shape_control=shape_control, coeff=coeff)
             elif selected_process == "Bayesian Run":
-                sample_number = st.number_input("Sample Number", value=0)
-                data = Bayesian_run(data, rbf_type=rbf_type, data_used=data_used, induct_used=induct_choice,
+                sample_number = st.number_input("Sample Number", value=2000)
+                data = Bayesian_run(data, rbf_type=rbf_type, data_used=data_used, induct_used=induct_used,
                                     der_used=der_used, cv_type=cv_type, reg_param=reg_param,
                                     shape_control=shape_control, coeff=coeff, NMC_sample=sample_number)
             elif selected_process == "BHT Run":
                 data = BHT_run(data, rbf_type, der_used, shape_control, coeff)
             elif selected_process == "Peak Analysis Run":
-                peak_method = st.selectbox("Peak Method", ["Option 1", "Option 2"])
-                N_peaks = st.number_input("Number of Peaks", value=0.0)
-                data = peak_analysis(data, rbf_type=rbf_type, data_used=data_used, induct_used=induct_choice,
+                peak_method = st.selectbox("Peak Method", ["separate", "combine"])
+                N_peaks = st.number_input("Number of Peaks", value=1)
+                data = peak_analysis(data, rbf_type=rbf_type, data_used=data_used, induct_used=induct_used,
                                      der_used=der_used, cv_type=cv_type, reg_param=reg_param,
                                      shape_control=shape_control, coeff=coeff, peak_method=peak_method, N_peaks=N_peaks)
 
